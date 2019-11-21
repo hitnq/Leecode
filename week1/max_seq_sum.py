0d0ce5004599c6cf8d2da9a3e42a0cc278d7437c
caijie12138 <774932729@qq.com>
2019-11-21 21:34:29 +0800

    week1

---
+++ week1/max_seq_sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_sum = nums[0]
+        cur_sum = 0
+        for i,num in enumerate(nums):
+            cur_sum += num
+            if cur_sum > max_sum:
+                max_sum = cur_sum
+            if cur_sum < 0:
+                cur_sum = 0
+        return max_sum
